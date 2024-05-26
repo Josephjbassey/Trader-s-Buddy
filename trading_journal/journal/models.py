52d18ac6a5c9b0ac3f0937205070a7fc6369ab84
Joseph Bassey John <136816366+Josephjbassey@users.noreply.github.com>
2024-05-26 23:04:40 +0000

    mod: modified model file

--- trading_journal/journal/models.py
+++ trading_journal/journal/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from wagtail.core.models import Page
+from wagtail.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 

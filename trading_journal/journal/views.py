4e9b11d2ad43f04c96ad1057b5c8c968d932d380
Joseph Bassey John <136816366+Josephjbassey@users.noreply.github.com>
2024-05-27 00:55:12 +0000

    Feature: Add view for risk calculator

--- trading_journal/journal/views.py
+++ trading_journal/journal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import RiskCalculatorForm
 
-# Create your views here.
+def risk_calculator(request):
+    form = RiskCalculatorForm()
+    position_size = None
+
+    if request.method == 'POST':
+        form = RiskCalculatorForm(request.POST)
+        if form.is_valid():
+            position_size = form.calculate_position_size()
+
+    return render(request, 'journal/risk_calculator.html', {'form': form, 'position_size': position_size})
